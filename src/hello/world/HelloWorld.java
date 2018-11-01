912cd96c1287525bb5e873755ca58139c4925b8f
TARUC <TARUC@PC-86>
2018-11-01 15:03:06 +0800

    update stupid Jiab

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -17,6 +17,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !");
         System.out.println("Hello World !!!!");
+        System.out.println("Hello World Jiab!!!!");
+        System.out.println("Hello World !!!!");
+        System.out.println("Hello World !!!!");
+        System.out.println("Hello World !!!!");
     }
     
 }
